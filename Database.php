fa22d3628592ae07707ab73faad1acae0921fb79
luisfer560 <luisfernando56uu@gmail.com>
2026-09-21 10:58:30 +0200

    Feat: update Database.php file

--- Database.php
+++ Database.php
@@ -1,6 +1,24 @@
 <?php 
+    class Database{
+        private $host = "localhost";
+        private $db_name = "luis_db";
+        private $username = "root";
+        private $password = "";
+        private $conn;
 
+        public function getConnection() {
+            $this->conn = null;
 
-
-
+            try {
+                $this->conn = new PDO(
+                    "mysql:host" . $this->host . "; db_name" . $this-> db_name,
+                    $this->username, 
+                    $this->password 
+                );
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+            } catch (PDOException $exception) {
+                echo "error de conexion: " . $exception->getMessage();
+            }
+        }
+    }
 ?>
